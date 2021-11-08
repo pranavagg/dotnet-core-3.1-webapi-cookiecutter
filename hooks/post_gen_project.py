fa95c9613187f0638886adb20f73cb1d8f5d5456
Pranav Aggarwal <pranav.aggarwal06@gmail.com>
2021-11-08 19:31:38 +0530

    Initial Commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,10 @@
+import subprocess
+
+solution_name = '{{cookiecutter.solution_name}}'
+
+# subprocess.check_call([f'dotnet new sln -o {solution_name}'])
+
+# api_name = '{{cookiecutter.api_name}}'
+
+# subprocess.check_call([f'dotnet sln add ./{api_name}/{api_name}.csproj'], cwd=solution_name)
+# subprocess.check_call([f'dotnet sln add ./{api_name}.Tests/{api_name}.Tests.csproj'], cwd=solution_name)
---
+++ {{cookiecutter.solution_name}}/{{cookiecutter.api_name}}.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace {{cookiecutter.api_name}}.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
